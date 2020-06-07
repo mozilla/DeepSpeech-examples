d76acaa2acbe2f9613aa4d705d3f284c5d0b6f10
j mike dupont <jmikedupont2@gmail.com>
2020-06-07 10:39:02 -0400

    adding readme

---
+++ batch_processing/Readme.md
@@ -0,0 +1,17 @@
+
+
+`pip3 install click delegator deepspeech`
+
+```
+C:\tools\Anaconda3\_conda.exe init powershell
+choco.exe install sox.portable ffmpeg
+```
+
+Websites referenced:
+https://deepspeech.readthedocs.io/en/v0.7.3/?badge=latest
+https://hacks.mozilla.org/2019/12/deepspeech-0-6-mozillas-speech-to-text-engine/
+https://palletsprojects.com/p/click/
+https://github.com/amitt001/delegator.py
+https://github.com/MichalMazurek/python-poetry/blob/d3f6df6a6c2587d7a6034719716de257917c4b0f/dockerfiles.py
+https://www.howtoforge.com/tutorial/ffmpeg-audio-conversion/
+https://www.programcreek.com/python/example/88033/click.Path
--- batch_processing/driver.py
+++ batch_processing/driver.py
@@ -1,9 +1,11 @@
-import os
 import glob
-import delegator
 import json
-import click
+import os
 from os.path import expanduser
+
+import click
+
+import delegator
 
 # first loop over the files
 # convert them to wave
@@ -13,66 +15,69 @@ from os.path import expanduser
 
 
 @click.command()
+@click.option("--dirname", type=click.Path(exists=True, resolve_path=True))
+@click.option("--ext")
 @click.option(
-    '--dirname',
-    type=click.Path(
-        exists=True,
-        resolve_path=True))
-
-@click.option( '--ext')
-
-@click.option( '--model', default="deepspeech-0.7.3-models.pbmm", type=click.FileName(exists=True, resolve_path=True))
-@click.option( '--scorer', default="deepspeech-0.7.3-models.scorer", type=click.FileName(exists=True, resolve_path=True))
+    "--model",
+    default="deepspeech-0.7.3-models.pbmm",
+    type=click.FileName(exists=True, resolve_path=True),
+)
+@click.option(
+    "--scorer",
+    default="deepspeech-0.7.3-models.scorer",
+    type=click.FileName(exists=True, resolve_path=True),
+)
 
 # manage my library of podcasts
-def main(dirname,ext, model, scorer):
+def main(dirname, ext, model, scorer):
     print("main")
-    model =  expanduser(model)
-    scorer =  expanduser(scorer)
-    pattern = dirname + "/"+ "*" + ext
+    model = expanduser(model)
+    scorer = expanduser(scorer)
+    pattern = dirname + "/" + "*" + ext
     audiorate = "16000"
-    
+
     print(pattern)
     for filename in glob.glob(pattern):
         print(filename)
 
         wavefile = filename + ".wav"
 
-        convert_command = " " . join([        
-            "ffmpeg",
-            "-i",
-            "'{}'".format(filename),
-            "-ar",
-            audiorate,
-            "'{}'".format(wavefile),
-
-            ])
+        convert_command = " ".join(
+            [
+                "ffmpeg",
+                "-i",
+                "'{}'".format(filename),
+                "-ar",
+                audiorate,
+                "'{}'".format(wavefile),
+            ]
+        )
         if not os.path.isfile(wavefile):
             print(convert_command)
             r = delegator.run(convert_command)
             print(r.out)
         else:
             print("skip")
-            
-        command = " " . join([
-            "deepspeech",
-            "--model",
-            model,
-            "--scorer",
-            scorer,
-            "--audio",
-            "'{}'".format(wavefile),
-            
-#            "--extended",
-            "--json"
-        ])
+
+        command = " ".join(
+            [
+                "deepspeech",
+                "--model",
+                model,
+                "--scorer",
+                scorer,
+                "--audio",
+                "'{}'".format(wavefile),
+                #            "--extended",
+                "--json",
+            ]
+        )
         print(command)
         r = delegator.run(command)
         with open(filename + ".json", "w") as fo:
             print(r.out)
             fo.write(r.out)
-        
-        
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()

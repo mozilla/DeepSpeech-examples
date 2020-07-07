42e234a6f0726cf689275e28a48f3a5794402239
Dan Steinman <dansteinman@gmail.com>
2020-07-06 22:41:26 -0400

    add electron example

---
+++ electron/public/electron.js
@@ -0,0 +1,24 @@
+const electron = require('electron');
+const app = electron.app;
+const path = require('path');
+const fs = require('fs');
+const createWindow = require('./create-window');
+const {getModel} = require('./recognize-wav');
+
+let appDataPath;
+
+if (fs.existsSync(path.resolve(__dirname, '../deepspeech-0.7.4-models.pbmm'))) {
+	// if the deepspeech model was found at the root, use that directory
+	appDataPath = path.resolve(__dirname, '..');
+}
+else {
+	// otherwise use the electron "appData" path
+	appDataPath = path.resolve(electron.app.getPath('appData'), 'deepspeech-electron');
+}
+
+app.on('ready', function () {
+	getModel(appDataPath, function (model) {
+		console.log('model loaded')
+		createWindow(model);
+	});
+});
---
+++ electron/public/recognize-wav.js
@@ -0,0 +1,64 @@
+const DeepSpeech = require('deepspeech');
+const fs = require('fs');
+const path = require('path');
+const wav = require('wav');
+const download = require('./download');
+
+// return the deepspeech model or download it if it is not found
+function getModel(appDataPath, callback) {
+	let modelPath = path.resolve(appDataPath, 'deepspeech-0.7.4-models.pbmm');
+	let scorerPath = path.resolve(appDataPath, 'deepspeech-0.7.4-models.scorer');
+	if (fs.existsSync(modelPath) && fs.existsSync(scorerPath)) {
+		callback(createModel(modelPath, scorerPath));
+	}
+	else {
+		// if the model files do not exist, download and save them to AppData path
+		console.log('\nDOWNLOADING MODEL TO: '+appDataPath+'\n');
+		const downloadPath = 'https://github.com/mozilla/DeepSpeech/releases/download/v0.7.4/deepspeech-0.7.4-models';
+		download(downloadPath+'.pbmm', modelPath, function() {
+			download(downloadPath+'.scorer', scorerPath, function() {
+				callback(createModel(modelPath, scorerPath));
+			});
+		});
+	}
+}
+
+// create the deepspeech model
+function createModel(modelPath, scorerPath) {
+	const model = new DeepSpeech.Model(modelPath);
+	model.enableExternalScorer(scorerPath);
+	return model;
+}
+
+// create a deepspeech stream to process a .wav file
+function recognizeWav(path, model) {
+	return new Promise(function(resolve, reject) {
+		try {
+			let modelStream = model.createStream();
+			const bufferSize = 512;
+			const file = fs.createReadStream(path, {highWaterMark: bufferSize});
+			const reader = new wav.Reader();
+			reader.on('format', function (format) {
+				if (format.sampleRate !== model.sampleRate()) {
+					reject(new Error('invalid sample rate: '+format.sampleRate));
+				}
+				reader.on('end', function () {
+					const results = modelStream.finishStream();
+					resolve(results);
+				});
+				reader.on('data', function (data) {
+					modelStream.feedAudioContent(data);
+				});
+			});
+			file.pipe(reader);
+		}
+		catch(e) {
+			reject(e);
+		}
+	});
+}
+
+module.exports = {
+	getModel,
+	recognizeWav
+};

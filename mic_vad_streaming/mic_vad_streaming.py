19797e0e593bbe64fb60ab6c9a3d0ec6482300e1
Reuben Morais <reuben.morais@gmail.com>
2020-08-02 17:27:21 +0200

    Handle rename to Mozilla Voice STT

--- mic_vad_streaming/mic_vad_streaming.py
+++ mic_vad_streaming/mic_vad_streaming.py
@@ -1,7 +1,7 @@
 import time, logging
 from datetime import datetime
 import threading, collections, queue, os, os.path
-import deepspeech
+import mozilla_voice_stt as stt
 import numpy as np
 import pyaudio
 import wave
@@ -160,7 +160,7 @@ def main(ARGS):
 
     print('Initializing model...')
     logging.info("ARGS.model: %s", ARGS.model)
-    model = deepspeech.Model(ARGS.model)
+    model = stt.Model(ARGS.model)
     if ARGS.scorer:
         logging.info("ARGS.scorer: %s", ARGS.scorer)
         model.enableExternalScorer(ARGS.scorer)
